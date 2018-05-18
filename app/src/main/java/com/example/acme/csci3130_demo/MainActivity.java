7166151ad6884994b47bb1ffbe09252bb1aa46d5
briankh <cyclode5@gmail.com>
2018-05-18 19:32:29 -0300

    k

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.acme.csci3130_demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void copyText(View v){
+        EditText entry = findViewById(R.id.editText);
+        TextView textView = findViewById(R.id.textView);
+        String curEntry = entry.getText().toString();
+        textView.setText(curEntry);
     }
 }
